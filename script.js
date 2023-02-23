d4deefc57fcf9778b0663018e3532e293b17f8e8
Ashish kumar Sahoo <kumarsahoo912@gmail.com>
2023-02-23 21:53:32 +0530

    b v

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 let yourArray = [true, 'two', 3, false, 'five'];
   let arrayOutput = document.getElementById('arrayOutput');
-  arrayOutput.innerHTML = 'Your array contains: ' + yourArray.join(', ');
+  arrayOutput.innerHTML =  'Your array contains: ' + yourArray.join(', ');
