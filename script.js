693ab5114f1503678124822269b79f56ca7d2d64
Ashish kumar Sahoo <kumarsahoo912@gmail.com>
2023-02-23 21:58:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-let yourArray = [true, 'two', 3, false, 'five'];
-  let arrayOutput = document.getElementById('arrayOutput');
-  arrayOutput.innerHTML =  'Your array contains: ' + yourArray.join(', ');
+const testData = [
+  { input: 'apple', expectedOutput: 'fruit' },
+  { input: 'carrot', expectedOutput: 'vegetable' },
+  { input: 'banana', expectedOutput: 'fruit' },
+  { input: 'lettuce', expectedOutput: 'vegetable' },
+];
+
+// Iterate over the test data array in your test
+testData.forEach((data) => {
+  it(`should correctly classify ${data.input} as a ${data.expectedOutput}`, () => {
+    cy.visit('/my-page');
+    cy.get('#input').type(data.input);
+    cy.get('#submit-button').click();
+    cy.get('#output').should('have.text', data.expectedOutput);
+  });
+});
